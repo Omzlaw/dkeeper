3ad9ea7bceb781e963cf3201aa99a642c6f915de
Omeiza Alabi <omeizaalabi@gmail.com>
2022-07-25 21:55:14 +0100

    Add react frontend and connect to blockchain backend

--- src/dkeeper_assets/src/components/App.jsx
+++ src/dkeeper_assets/src/components/App.jsx
@@ -1,19 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
+import {dkeeper} from "../../../declarations/dkeeper";
 
 function App() {
   const [notes, setNotes] = useState([]);
 
+  useEffect(() => {
+    fetchData();
+  }, [])
+  
+  async function fetchData() {
+    const notesArray = await dkeeper.readNotes();
+    setNotes(notesArray);
+  }
+
   function addNote(newNote) {
     setNotes(prevNotes => {
-      return [...prevNotes, newNote];
+      dkeeper.createNote(newNote.title, newNote.content);
+      return [newNote, ...prevNotes];
     });
   }
 
   function deleteNote(id) {
+    dkeeper.removeNote(id);
     setNotes(prevNotes => {
       return prevNotes.filter((noteItem, index) => {
         return index !== id;
